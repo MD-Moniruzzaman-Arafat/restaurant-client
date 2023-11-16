a98f91a70b265a8b0b42fd1e2028ed6743545233
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-11-16 21:42:33 +0600

    react tab

--- src/page/OurShop/OurShop.jsx
+++ src/page/OurShop/OurShop.jsx
@@ -1,10 +1,37 @@
 import Banner from "../../components/Banner/Banner";
 import img from "../../assets/shop/banner2.jpg"
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
+import MenuItemhook from "../../hooks/menuItem/MenuItemhook";
+import { useState } from "react";
 
 const OurShop = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menus] = MenuItemhook()
+    const offered = menus.filter(data => data.category == 'offered')
+    const dessert = menus.filter(data => data.category == 'dessert')
+    const pizza = menus.filter(data => data.category == 'pizza')
+    const salad = menus.filter(data => data.category == 'salad')
+    const soup = menus.filter(data => data.category == 'soup')
+    const drinks = menus.filter(data => data.category == 'drinks')
+
     return (
         <div>
             <Banner img={img} title={"OUR SHOP"} description={"Would you like to try a dish?"}></Banner>
+            <Tabs className={"text-center my-10"} defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>SOUP</Tab>
+                    <Tab>SALAD</Tab>
+                    <Tab>PIZZA</Tab>
+                    <Tab>DESSERT</Tab>
+                    <Tab>DRINKS</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
