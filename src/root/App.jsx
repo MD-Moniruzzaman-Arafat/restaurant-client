17113c6893f0e059fee145cc09501fc0da9c3978
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-11-16 13:49:25 +0600

    footer section

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar/Navbar'
 import './App.css'
+import Footer from '../components/Footer/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   )
 }
