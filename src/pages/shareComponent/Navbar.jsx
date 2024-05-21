43cfcede285e0ad80bc8ce44bfb3815d1eee8c15
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-05-21 10:21:34 +0600

    customize  navbar component

--- src/pages/shareComponent/Navbar.jsx
+++ src/pages/shareComponent/Navbar.jsx
@@ -1,4 +1,27 @@
 export default function Navbar() {
+  const menu = (
+    <>
+      <li>
+        <a>HOME</a>
+      </li>
+
+      <li>
+        <a>CONTACT US</a>
+      </li>
+      <li>
+        <a>DASHBOARD</a>
+      </li>
+      <li>
+        <a>OUR MENU</a>
+      </li>
+      <li>
+        <a>OUR SHOP</a>
+      </li>
+      <li>
+        <a>SIGN OUT</a>
+      </li>
+    </>
+  );
   return (
     <div>
       <div className="navbar bg-base-100">
@@ -24,52 +47,13 @@ export default function Navbar() {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menu}
             </ul>
           </div>
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{menu}</ul>
         </div>
       </div>
     </div>
