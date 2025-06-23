3ec0828e512887a5be9187169aa525c249e0b8bd
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-06-23 13:11:34 +0600

    footer section design

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer'
 
 export default function Main() {
     return (
         <div>
             <Outlet />
+            <Footer />
         </div>
     )
 }
