c1206351c01eded574bea1c801479f3b49643d81
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-06-25 21:57:26 +0600

    react helmet use

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,10 +6,14 @@ import FormOurMenu from '../../components/FormOurMenu/FormOurMenu'
 import ChefRecommends from '../../components/ChefRecommends/ChefRecommends'
 import FeatureItem from '../../components/FeatureItem/FeatureItem'
 import Testimonial from '../../components/Testimonial/Testimonial'
+import { Helmet } from 'react-helmet'
 
 export default function Home() {
     return (
         <div>
+            <Helmet>
+                <title>Restaurant | Home</title>
+            </Helmet>
             <Banner />
             <OrderOnline />
             <SectionBanner />
--- src/pages/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu.jsx
@@ -4,10 +4,14 @@ import TodaysOffer from '../../components/TodaysOffer/TodaysOffer'
 import Desserts from '../../components/Desserts/Desserts'
 import Pizza from '../../components/Pizza/Pizza'
 import Soups from '../../components/Soups/Soups'
+import { Helmet } from 'react-helmet'
 
 export default function OurMenu() {
     return (
         <>
+            <Helmet>
+                <title>Restaurant | Our-Menu</title>
+            </Helmet>
             <OurMenuBanner />
             <TodaysOffer />
             <Desserts />
